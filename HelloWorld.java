a4eb981adb7792e182d2a271c94bcb6f428c83fe
lisbobo <wendellisboa@outlook.com.br>
2024-03-14 03:30:12 -0400

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        String ambiente = args[0];
+        if (ambiente.equals("DEV")) {
+            System.out.print("Executando em DEV");
+        } else if (ambiente.equals("TESTE")) {
+            System.out.print("Executando em TESTE");
+        } else {
+            System.out.print(ambiente);
+        }
+    }
+}
