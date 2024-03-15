a7e8595e996a6091e70c4732b295b9003b4f63a5
lisbobo <wendellisboa@outlook.com.br>
2024-03-15 10:23:06 -0400

    att

---
+++ Modulo 8/Tarefa.java
@@ -0,0 +1,20 @@
+public class Tarefa {
+
+    public static void main(String args[]) {
+        tarefaAritmeticas();
+
+    }
+
+    public static void tarefaAritmeticas() {
+
+        int nota1 = 20;
+        int nota2 = 30;
+        int nota3 = 20;
+        int nota4 = 40;
+
+        int media = (nota1 + nota2 + nota3 + nota4) / 4;
+        System.out.println("A média das 4 notas é: " + media);
+
+    }
+
+}
